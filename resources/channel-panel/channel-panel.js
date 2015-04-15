a70e75a09295f890c41eb0d05c25bbf51f8366a1
Andrea Stella <adr.stella@gmail.com>
2015-04-15 12:07:37 -0600

    channel-panel taking shape

--- resources/channel-panel/channel-panel.js
+++ resources/channel-panel/channel-panel.js
@@ -2,7 +2,7 @@ angular.module('tc').directive('channelPanel', ['settings', function(settings) {
 	return {
 		restrict: 'E',
 		templateUrl: 'resources/channel-panel/channel-panel.html',
-		link: function(scope, element) {
+		link: function(scope) {
 			scope.settings = settings;
 		}
 	}
