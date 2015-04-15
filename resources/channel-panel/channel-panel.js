40719fd4df4b2fab30b902db1d49b13628b39b2b
Andrea Stella <adr.stella@gmail.com>
2015-04-15 13:31:37 -0600

    channel-panel buttons now functional

--- resources/channel-panel/channel-panel.js
+++ resources/channel-panel/channel-panel.js
@@ -1,9 +1,32 @@
-angular.module('tc').directive('channelPanel', ['settings', function(settings) {
+angular.module('tc').directive('channelPanel', ['settings', 'gui', '$filter', function(settings, gui, $filter) {
+	
+	function link(scope) {
+		scope.settings = settings;
+
+		scope.channel = function() {
+			return settings.channels[settings.selectedTabIndex]
+		};
+
+		scope.broadcaster = function () {
+			return $filter('stripHash')(scope.channel());
+		};
+
+		scope.leave = function() {
+			settings.channels.splice(settings.selectedTabIndex, 1);
+		};
+
+		scope.openStream = function() {
+			gui.Shell.openExternal('http://www.twitch.tv/'+scope.broadcaster()+'/popout');
+		};
+
+		scope.openChannel = function() {
+			gui.Shell.openExternal('http://www.twitch.tv/'+scope.broadcaster());
+		};
+	}
+	
 	return {
 		restrict: 'E',
 		templateUrl: 'resources/channel-panel/channel-panel.html',
-		link: function(scope) {
-			scope.settings = settings;
-		}
+		link: link
 	}
 }]);
--- resources/providers/gui.js
+++ resources/providers/gui.js
@@ -1,3 +1,3 @@
 angular.module('tc').factory('gui', function() {	
-	return var gui = require('nw.gui');
+	return require('nw.gui');
 });
