f42f36a14a92d5b30349c9ad9020298deda94c7c
Andrea Stella <adr.stella@gmail.com>
2015-04-24 13:10:52 -0600

    move non angular code out of ng folder

---
+++ assets/js/nw-setup.js
@@ -0,0 +1,12 @@
+var gui = require('nw.gui');
+/*
+// Create a tray icon
+var tray = new gui.Tray({ title: 'Tray', icon: 'assets/img/twitch.png' });
+var win = gui.Window.get();
+
+console.log('window', window);
+console.log('Window', Window);
+
+win.on('close', function() {
+	win.hide();
+});*/
