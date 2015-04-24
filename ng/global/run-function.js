7327c217a15fef161ca47278c70fcfdacc390ec3
Andrea Stella <adr.stella@gmail.com>
2015-04-24 15:41:17 -0600

    rudimentary tray icon, closes #15

---
+++ ng/global/run-function.js
@@ -0,0 +1,27 @@
+/**
+ * This piece of code gets executed when loading the application.
+ * It contains nwjs setup and behavior.
+ */
+angular.module('tc').run(['gui', function(gui) {
+	var win = gui.Window.get();
+	var tray = new gui.Tray({ title: 'Tray', icon: 'assets/img/twitch.png' });
+	var shown = true;
+
+	tray.on('click', function() {
+		shown = !shown;
+		win.show(shown);
+	});
+
+	win.on('close', function() {
+		if (confirm('You will disconnect from the server'))	{
+			win.hide(); // snappier perceived performance
+			tray.remove();
+			win.close(true);
+		}
+	});
+
+	win.on('minimize', function() {
+		shown = false;
+		win.hide();
+	});
+}]);
