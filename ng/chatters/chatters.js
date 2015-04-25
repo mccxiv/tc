b24510e6e0e9382e8ac7d9f516da802c665bac23
Andrea Stella <adr.stella@gmail.com>
2015-04-25 15:20:31 -0600

    less wasteful viewer list auto refreshing: only active tab is refreshed

--- ng/chatters/chatters.js
+++ ng/chatters/chatters.js
@@ -1,4 +1,4 @@
-angular.module('tc').directive('chatters', ['$http', '$filter', function($http, $filter) {
+angular.module('tc').directive('chatters', ['$http', '$filter', 'settings', function($http, $filter, settings) {
 	
 	function makeListUrl(broadcaster) {
 		return 'https://tmi.twitch.tv/group/user/'+broadcaster+'/chatters?callback=JSON_CALLBACK';
@@ -6,44 +6,63 @@ angular.module('tc').directive('chatters', ['$http', '$filter', function($http, 
 	
 	function link(scope) {
 		var forceShowViewers = false;
-		var timeout = null;
-		scope.api = null;
-		scope.showViewers = function(force) {
-			if (typeof force === 'boolean') forceShowViewers = force;
-			if (!scope.api) return false;
-			if (scope.api.chatters.viewers.length < 201) return true;
-			else return forceShowViewers;
-		};
+		var timeoutReload = null;
+		var timeoutOnSelect = null;
 		
-		fetchList();
+		scope.api = null;
+		scope.showViewers = showViewers;
+
+		/**
+		 * Fetches viewer list when this channel is selected.
+		 * Avoids useless calls by waiting to see if the user sticks around
+		 */
+		onChannelSelected(function() {
+			if (!scope.api) fetchList();
+			else timeoutOnSelect = setTimeout(fetchList, 2000)
+		});
 		
 		function fetchList() {
-			console.log('CHATTERS: Getting user list for channel '+scope.channel);
-			
+			if (!isChannelSelected()) return; // Abort
+			console.log('CHATTERS: Getting user list for channel '+scope.channel);			
 			var broadcaster = $filter('stripHash')(scope.channel);
 			var url = makeListUrl(broadcaster);
 			var req = $http.jsonp(url);
 			req.success(onList);
 			req.error(onListError);
-			req.finally(fetchListDelayed);
-			return req;
+			timeoutReload = setTimeout(fetchList, 120000);
 		}		
 		
 		function onList(result, status) {
 			if (result && result.data && result.data.chatters) {
-				console.log('CHATTERS: Got viewer list', result.data);
+				console.log('CHATTERS: Got viewer list for '+scope.channel, result.data);
 				scope.api = result.data;
 			}
 			else onListError(result, status);
 		}
 		
-		function fetchListDelayed() {
-			timeout = setTimeout(fetchList, 120000)
-		}
-		
 		function onListError(result, status) {
 			console.warn('NYI Error'); // TODO
 		}
+		
+		function isChannelSelected() {
+			return settings.channels[settings.selectedTabIndex] === scope.channel;
+		}
+		
+		function onChannelSelected(cb) {
+			scope.$watch(
+				function() {return settings.selectedTabIndex},
+				function(o, n) {
+					if (isChannelSelected()) cb();
+				}
+			);
+		}
+		
+		function showViewers(force) {
+			if (typeof force === 'boolean') forceShowViewers = force;
+			if (!scope.api) return false;
+			if (scope.api.chatters.viewers.length < 201) return true;
+			else return forceShowViewers;
+		}		
 	}
 
 	return {
