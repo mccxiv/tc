598ba980793d212c3fc14762c63030c7e80a84de
Andrea Stella <adr.stella@gmail.com>
2015-04-25 18:22:26 -0600

    support all twitch slash (dot in irc) commands, closes #9

--- ng/chat-input/chat-input.js
+++ ng/chat-input/chat-input.js
@@ -5,6 +5,12 @@ angular.module('tc').directive('chatInput', ['settings', 'irc', function(setting
 		
 		scope.say = function() {
 			var channel = settings.channels[settings.selectedTabIndex];
+			if (!channel) return;
+			
+			if (scope.message.charAt(0) === '/') {
+				scope.message = '.' + scope.message.substr(1);
+			}
+			
 			irc.say(channel, scope.message);
 			scope.message = '';
 		};
