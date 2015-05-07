a22d3e24c745add31b52053cf1baaed7aee41800
Andrea Stella <adr.stella@gmail.com>
2015-05-07 14:32:41 -0600

    fix settings-panel rendering issue

--- ng/settings-panel/settings-panel.js
+++ ng/settings-panel/settings-panel.js
@@ -7,16 +7,12 @@
 angular.module('tc').directive('settingsPanel', function(settingsGui, $compile) {
 
 	function main(scope, element) {
+		element.attr('layout', 'row');
 		scope.m = {selected: 0};
 		scope.items = settingsGui.getItems();
 		scope.compile = function(html) {
 			return $compile(html)(scope);
-			console.log('rendered!');
 		};
-
-		element.attr('layout', 'row');
-
-		window.sp = scope;
 	}
 
 	return {
