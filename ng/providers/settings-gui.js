98742cd85f02d82acbc4709a6f25460d700a83c2
Andrea Stella <adr.stella@gmail.com>
2015-05-07 18:55:50 -0600

    add close button to options screen, closes #74

--- ng/providers/settings-gui.js
+++ ng/providers/settings-gui.js
@@ -8,14 +8,17 @@ angular.module('tc').factory('settingsGui', function($mdDialog, $rootElement) {
 	return {
 		/**
 		 * Shows the options page.
-		 * @param $event - Angular event used for the enter animation
+		 * @param $event - Angular event used for the enter and exit animations
 		 */
 		show: function($event) {
 			$mdDialog.show({
 				parent: $rootElement,
 				targetEvent: $event,
 				templateUrl: 'ng/settings-panel/settings-gui-dialog.html',
-				clickOutsideToClose: true
+				clickOutsideToClose: true,
+				controller: function(scope, $mdDialog) {
+					scope.close = $mdDialog.hide;
+				}
 			});
 		}
 	}
