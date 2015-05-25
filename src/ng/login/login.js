6df75974d6802f0c30e98b6f0da9c353e92932a9
Andrea Stella <adr.stella@gmail.com>
2015-05-24 23:10:59 -0600

    add link to generate oauth password

--- src/ng/login/login.js
+++ src/ng/login/login.js
@@ -1,15 +1,21 @@
-angular.module('tc').directive('login', ['settings', 'irc', function(settings, irc) {
+angular.module('tc').directive('login', function(settings, irc, gui) {
 	return {
 		restrict: 'E',
 		templateUrl: 'ng/login/login.html',
 		link: function(scope) {
 			scope.irc = irc;
 			scope.settings = settings;
+
 			scope.login = function() {
 				settings.identity.username = scope.m.username;
 				settings.identity.password = scope.m.password;
 				console.log('LOGIN: User supplied credentials.', settings.identity.username, settings.identity.password);
 			};
+
+			scope.generate = function() {
+				gui.Shell.openExternal('http://twitchapps.com/tmi/');
+			};
+
 			// These values should NOT update the settings object or
 			// it will break the form's conditionals
 			scope.m = {};
@@ -19,4 +25,4 @@ angular.module('tc').directive('login', ['settings', 'irc', function(settings, i
 			scope.m.haveNoPassword = !settings.identity.password.length;		
 		}
 	}
-}]);
+});
