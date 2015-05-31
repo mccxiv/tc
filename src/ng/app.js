615a904f621cc0d7d692c5c9782f74e4456d7b20
Andrea Stella <adr.stella@gmail.com>
2015-05-31 17:02:00 -0600

    add access to dev tools in built version with F12, disable js uglifier because it's breaking things

--- src/ng/app.js
+++ src/ng/app.js
@@ -1,2 +1,7 @@
-//require('nw.gui').Window.get().showDevTools();
 angular.module('tc', ['ngMaterial', 'ngSanitize']);
+
+$('body').on('keyup', function(e) {
+	if (e.which === 123) {
+		require('nw.gui').Window.get().showDevTools();
+	}
+});
