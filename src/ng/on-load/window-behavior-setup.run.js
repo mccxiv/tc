00a9fc6b9e15d22a5da94d48d91fc91c9b08979a
Andrea Stella <adr.stella@gmail.com>
2015-06-06 15:57:29 -0600

    fix syntax error, fix Tc case

--- src/ng/on-load/window-behavior-setup.run.js
+++ src/ng/on-load/window-behavior-setup.run.js
@@ -14,11 +14,11 @@ angular.module('tc').run(function(gui, settings) {
 	}));
 	tray.menu = menu;
 	
-	if(process.platform === 'darwin') {
-+               var osxbar = new gui.Menu({ type: "menubar" });
-+               osxbar.createMacBuiltin("TC");
-+               win.menu = osxbar;
-+       }
+	if (process.platform === 'darwin') {
+		var osxbar = new gui.Menu({type: 'menubar'});
+		osxbar.createMacBuiltin('Tc');
+		win.menu = osxbar;
+	}
 
 	tray.on('click', function() {
 		shown = !shown;
