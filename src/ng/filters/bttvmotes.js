85e7572dc6a7ff23a2390b7bcf0b742d6163627b
Andrea Stella <adr.stella@gmail.com>
2015-06-30 17:39:59 -0600

    text after BTTV emotes now shows correctly, fixes #142

--- src/ng/filters/bttvmotes.js
+++ src/ng/filters/bttvmotes.js
@@ -28,6 +28,8 @@ angular.module('tc').filter('bttvmotes', function(bttv) {
 		var newParts = [];
 
 		parts.forEach(function(part) {
+
+			// if it's not plain text, ignore this bit
 			if (part.isElement) {
 				newParts.push(part);
 				return;
@@ -37,23 +39,26 @@ angular.module('tc').filter('bttvmotes', function(bttv) {
 			var string = part.string;
 			var match;
 
+			// for each character sequence that could potentially be a string
 			while ((match = potentialEmoteRegex.exec(string)) !== null) {
 				// TODO refactor inefficient algorithm
+
+				// if it's indeed an emote
 				if (isEmote(match[0], emotes)) {
 					console.log('BTTVMOTES: Emote found', match[0]);
 
 					// Save previous bit as a non emote
 					if (match.index > endIndexOfPreviousEmote) {
-						var before = string.substr(endIndexOfPreviousEmote, match.index);
+						var before = string.substring(endIndexOfPreviousEmote, match.index);
 						add(before, false);
 					}
 
-					// Save emote as tag
+					// Save emote as tag element
 					var img = makeEmote(_.find(emotes, 'emote', match[0]).url);
 					add(img, true);
 
 					// Track progress through string
-					endIndexOfPreviousEmote = match.lastIndex;
+					endIndexOfPreviousEmote = potentialEmoteRegex.lastIndex;
 				}
 			}
 
