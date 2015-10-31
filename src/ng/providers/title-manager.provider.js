0bd09114ce6b15c4c6c3f1fdf746025a8d92b7ed
Andrea Stella <adr.stella@gmail.com>
2015-10-31 12:58:00 -0600

    Replace nwjs API calls

--- src/ng/providers/title-manager.provider.js
+++ src/ng/providers/title-manager.provider.js
@@ -4,11 +4,11 @@
  * @ngdoc factory
  * @name titleManager
  */
-angular.module('tc').factory('titleManager', function($filter, channels, settings) {
+angular.module('tc').factory('titleManager', function(
+	$filter, channels, settings) {
 	console.log('LOAD: titleManager');
 
 	var capitalize = $filter('capitalize');
-	var win = nw.Window.get();
 
 	channels.on('change', function() {
 		var prefix;
@@ -16,7 +16,7 @@ angular.module('tc').factory('titleManager', function($filter, channels, setting
 		var channel = settings.channels[settings.selectedTabIndex];
 		if (channel) prefix = capitalize(channel);
 		else prefix = 'Join channel';
-		win.title = prefix+suffix;
+		document.title = prefix + suffix;
 	});
 
 	return null;
