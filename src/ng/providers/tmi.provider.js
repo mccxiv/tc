03cca49ddbdfab2ae707cc35e8caf30b7409f322
Andrea Stella <adr.stella@gmail.com>
2015-10-31 16:46:21 -0600

    Switch to node tmi, less bug prone

--- src/ng/providers/tmi.provider.js
+++ src/ng/providers/tmi.provider.js
@@ -6,6 +6,12 @@
  */
 angular.module('tc').factory('tmi', function() {
 	console.log('LOAD: tmi');
-	//require('tmi.js'); Using browser version for now...
-	return window.irc;
+
+	var tmi = require('tmi.js');
+	if (typeof tmi.client !== 'function') {
+		console.warn('Node tmi.js thinks we\'re in the browser.');
+		console.warn('Using workaround...');
+		tmi = window.irc;
+	}
+	return tmi;
 });
