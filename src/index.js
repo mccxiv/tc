6cb2aef78ffc5ac820dbb1f7eec542be71c68c7c
Andrea Stella <adr.stella@gmail.com>
2015-11-02 14:46:29 -0600

    Add notifications on Windows

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var app = require('app');
 var ipc = require('ipc');
 var path = require('path');
+var Tray = require('tray');
 var argv = require('yargs').argv;
 var BrowserWindow = require('browser-window');
 var startup = require('./assets/squirrel-startup.js');
@@ -8,14 +9,17 @@ var startup = require('./assets/squirrel-startup.js');
 var DATA_PATH = path.resolve(argv.data || path.resolve(__dirname, './settings'));
 
 var main;
+var tray;
 
 if (startup()) return;
 app.setPath('userData', DATA_PATH);
-app.on('ready', ready);
+app.on('ready', makeWindow);
+app.on('ready', makeTray);
 ipc.on('open-dev-tools', devTools);
+ipc.on('notification', notification);
 app.on('window-all-closed', app.quit.bind(app));
 
-function ready() {
+function makeWindow() {
 	main = new BrowserWindow({
 		width: 800,
 		height: 450,
@@ -29,6 +33,15 @@ function ready() {
 	main.on('closed', function() {
 		main = null;
 	});
+}
+
+function makeTray() {
+	tray = new Tray(__dirname + '/assets/icon16.png');
+}
+
+function notification(e, obj) {
+	if (process.platform === 'win32') tray.displayBalloon(obj);
+	else console.warn('Notifications only supported on Windows');
 }
 
 function devTools() {
