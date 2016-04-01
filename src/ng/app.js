4f2e565dbce470c5cc7151bd0fe0a8433c04d3b0
Andrea Stella <adr.stella@gmail.com>
2016-04-01 15:53:43 -0700

    Move css where it belongs

--- src/ng/app.js
+++ src/ng/app.js
@@ -1,8 +1,4 @@
 angular.module('tc', ['ngMaterial', 'ngSanitize']);
 
-setTimeout(function() {
-  angular.bootstrap(document, ['tc']);
-}, 2400);
-
-$('.loader').delay(4400).fadeOut(1000);
-$('.app').delay(4400).fadeTo(1000, 1);
+$('.loader').delay(1400).fadeOut(1000);
+$('.app').delay(1400).fadeTo(1000, 1);
