1664d807503976a5d4222e82873c76c217905398
Andrea Stella <adr.stella@gmail.com>
2016-03-31 17:44:07 -0700

    Fix indent

--- src/index.js
+++ src/index.js
@@ -24,86 +24,86 @@ app.on('before-quit', app.exit); // Skip the 'close' event
 app.on('activate', unhideAppOnMac);
 
 function makeWindow() {
-	var mainWinState = windowState({
-		defaultWidth: 800,
-		defaultHeight: 450
-	});
+  var mainWinState = windowState({
+    defaultWidth: 800,
+    defaultHeight: 450
+  });
 
-	main = new BrowserWindow({
-		x: mainWinState.x,
-		y: mainWinState.y,
-		width: mainWinState.width,
-		height: mainWinState.height,
-		'min-width': 500,
-		'min-height': 390,
-		icon: __dirname + '/assets/icon256.png'
-	});
+  main = new BrowserWindow({
+    x: mainWinState.x,
+    y: mainWinState.y,
+    width: mainWinState.width,
+    height: mainWinState.height,
+    'min-width': 500,
+    'min-height': 390,
+    icon: __dirname + '/assets/icon256.png'
+  });
 
-	mainWinState.manage(main);
+  mainWinState.manage(main);
 
-	main.on('close', function(e) {
-		console.log('TC: Window tried closing, hiding it instead.');
-		e.preventDefault();
-		if (process.platform === 'darwin') app.hide();
-		else main.hide();
-	});
+  main.on('close', function (e) {
+    console.log('TC: Window tried closing, hiding it instead.');
+    e.preventDefault();
+    if (process.platform === 'darwin') app.hide();
+    else main.hide();
+  });
 
-	main.setMenu(null);
-	main.loadUrl('file://' + __dirname + '/index.html');
+  main.setMenu(null);
+  main.loadUrl('file://' + __dirname + '/index.html');
 }
 
 function devTools() {
-	main.openDevTools();
+  main.openDevTools();
 }
 
 function isSecondInstance() {
-	return app.makeSingleInstance(secondaryInstanceLaunched);
+  return app.makeSingleInstance(secondaryInstanceLaunched);
 }
 
 // Called when this is the main instance, and another is launched
 function secondaryInstanceLaunched() {
-	if (main.isMinimized()) main.restore();
-	if (!main.isVisible()) main.show();
-	if (!main.isFocused()) main.focus();
+  if (main.isMinimized()) main.restore();
+  if (!main.isVisible()) main.show();
+  if (!main.isFocused()) main.focus();
 }
 
 function enableAutoStart() {
-	toggleAutostart(true);
+  toggleAutostart(true);
 }
 
 function disableAutoStart() {
-	toggleAutostart(false);
+  toggleAutostart(false);
 }
 
 function toggleAutostart(adding) {
-	if (process.platform === 'win32') {
-		var command = adding? 'createShortcut' : 'removeShortcut';
+  if (process.platform === 'win32') {
+    var command = adding ? 'createShortcut' : 'removeShortcut';
 
-		var fs = require('fs');
-		var path = require('path');
-		var execSync = require('child_process').execSync;
-		var target = path.basename(process.execPath);
-		var updateDotExe = path.resolve(
-				path.dirname(process.execPath),
-				'..',
-				'update.exe'
-		);
+    var fs = require('fs');
+    var path = require('path');
+    var execSync = require('child_process').execSync;
+    var target = path.basename(process.execPath);
+    var updateDotExe = path.resolve(
+      path.dirname(process.execPath),
+      '..',
+      'update.exe'
+    );
 
-		var createShortcut = '"' + updateDotExe + '"' +
-				' --'+command+'="' + target + '"' +
-				' --shortcut-locations=Startup';
+    var createShortcut = '"' + updateDotExe + '"' +
+      ' --' + command + '="' + target + '"' +
+      ' --shortcut-locations=Startup';
 
-		// Check that Update.exe exists, otherwise we're in standalone mode
-		fs.stat(updateDotExe, function(err, stats) {
-			if (err || !stats.size) console.warn('TC: Update.exe not found.');
-			else execSync(createShortcut);
-		});
+    // Check that Update.exe exists, otherwise we're in standalone mode
+    fs.stat(updateDotExe, function (err, stats) {
+      if (err || !stats.size) console.warn('TC: Update.exe not found.');
+      else execSync(createShortcut);
+    });
 
-	}
-	else console.warn('TC: There is no autostart option for this platform.');
+  }
+  else console.warn('TC: There is no autostart option for this platform.');
 }
 
-function unhideAppOnMac()  {
-	console.log('TC: App activated, unhiding the window.');
-	app.show();
+function unhideAppOnMac() {
+  console.log('TC: App activated, unhiding the window.');
+  app.show();
 }
