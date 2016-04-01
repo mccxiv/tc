e031054b93d5e57bbe6f9447ab763fd4302c9f37
Andrea Stella <adr.stella@gmail.com>
2016-04-01 00:42:37 -0700

    Add loading animation, closes #226

--- src/ng/app.js
+++ src/ng/app.js
@@ -1,1 +1,8 @@
 angular.module('tc', ['ngMaterial', 'ngSanitize']);
+
+setTimeout(function() {
+  angular.bootstrap(document, ['tc']);
+}, 2400);
+
+$('.loader').delay(4400).fadeOut(1000);
+$('.app').delay(4400).fadeTo(1000, 1);
