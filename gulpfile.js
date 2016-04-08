507ca3753b8905777295322fdd65097dbe8a7e09
Andrea Stella <adr.stella@gmail.com>
2016-04-07 18:53:33 -0700

    Make mac build work

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,17 @@ gulp.task('reinstall', function() {
 gulp.task('cleanup', function() {
   shell.mkdir('-p', '_dist');
   shell.mv('dist/win-x64/**', '_dist/');
-  shell.rm('-rf', 'dist');
+  shell.mv('dist/Tc-darwin-x64/*.dmg', '_dist/');
+  shell.mv('dist/Tc-darwin-x64/*.zip', '_dist/');
   shell.find('_dist').filter((f) => f.endsWith('.exe')).forEach((f) => {
     shell.mv(f, f.replace('TcSetup', 'tc-setup-windows'));
   });
+  shell.find('_dist').filter((f) => f.endsWith('.dmg')).forEach((f) => {
+    shell.mv(f, f.replace('Tc', 'tc-setup-mac'));
+  });
+  shell.find('_dist').filter((f) => f.endsWith('.zip')).forEach((f) => {
+    var newName = f.replace('-mac', '').replace('Tc', 'tc-mac');
+    shell.mv(f, newName);
+  });
+  shell.rm('-rf', 'dist');
 });
