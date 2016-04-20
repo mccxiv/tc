58dbaa67b298af44299d87fa113ce686c9f900f1
Andrea Stella <adr.stella@gmail.com>
2016-04-20 11:33:40 -0600

    Fix change event

--- src/tc-renderer/lib/channels.js
+++ src/tc-renderer/lib/channels.js
@@ -9,6 +9,12 @@ emitter.setMaxListeners(0);
 emitter.channels = channels;
 emitter.current = () => channels[settings.selectedTabIndex];
 
+Object.observe(settings, (changes) => {
+  changes.forEach((change) => {
+    if (change.name === 'selectedTabIndex') emitter.emit('change');
+  });
+});
+
 Array.observe(channels, ([change]) => {
   emitter.emit('change');
   if (change.addedCount) emitter.emit('add', channels[change.index]);
