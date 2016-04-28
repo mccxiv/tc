2d22bdf6dc2a63d1f40c07198fde1545c833b367
Andrea Stella <adr.stella@gmail.com>
2016-04-28 12:49:08 -0600

    Fix emotes with fancy characters like <

--- src/tc-renderer/lib/transforms/add-emotes-as-images.js
+++ src/tc-renderer/lib/transforms/add-emotes-as-images.js
@@ -1,13 +1,14 @@
+import escape from './escape';
 import escapeRegex from 'lodash.escaperegexp';
 
 function makeImg(emote) {
-  const meta = `data-emote-name="${emote.emote}"`;
+  const meta = `data-emote-name="${emote.emote}" alt="${emote.emote}"`;
   return `<img class="emoticon" ${meta} src="${emote.url}">`;
 }
 
 export default function addEmotesAsImages(message, emotes) {
   emotes.forEach((e) => {
-    const escaped = escapeRegex(e.emote);
+    const escaped = escapeRegex(escape(e.emote));
     const regString = `(?<=(?:^| ))${escaped}(?=(?: |$))(?!(?:[^<]*>))`;
     const re = new RegExp(regString, 'g');
     message = message.replace(re, makeImg(e));
