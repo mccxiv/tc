9e804dabf13998ca9352f03d37811bce6b0d2ac8
Andrea Stella <adr.stella@gmail.com>
2016-05-20 12:13:07 -0600

    Copy BTTV's url parser, fixes  #255

--- src/tc-renderer/lib/transforms/add-links.js
+++ src/tc-renderer/lib/transforms/add-links.js
@@ -1,11 +1,16 @@
-const regex = /\S*\w+\.[a-zA-Z]{2,63}\S*/g;
+// Regex taken from From Night's BTTV
+// https://github.com/night/BetterTTV/
 
-function makeAnchor(url) {
-  const original = url;
-  if (!/^(http|https):\/\//.test(url)) url = 'http://' + url;
-  return `<a href="${url}">${original}</a>`;
-}
+const regex = /(?:https?:\/\/)?(?:[-a-zA-Z0-9@:%_\+~#=]+\.)+[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/\/=()]*)/gi;
 
-export default function addLinks(msg) {
-  return msg.replace(regex, match => makeAnchor(match));
+export default function addLinks(message) {
+  return message.replace(regex, function(e) {
+    if (  e.indexOf('@') > -1
+          && (e.indexOf('/') === -1
+          || e.indexOf('@') < e.indexOf('/'))) {
+      return '<a href="mailto:' + e + '">' + e + '</a>';
+    }
+    var link = e.replace(/^(?!(?:https?:\/\/|mailto:))/i, 'http://');
+    return `<a href="${link}" target="_blank">${e}</a>`;
+  });
 }
