c058c847b9465dc9312cd8d20ca15835c448f00c
mccxiv <adr.stella@gmail.com>
2016-06-18 18:06:39 -0600

    Show channel in title bar when opening app, fixes #266

--- src/tc-renderer/lib/startup/keep-title-updated.js
+++ src/tc-renderer/lib/startup/keep-title-updated.js
@@ -3,12 +3,16 @@ import settings from '../settings/settings';
 import capitalize from '../transforms/capitalize';
 
 export default function keepTitleUpdated() {
-  channels.on('change', () => {
+
+  update();
+  channels.on('change', update);
+
+  function update() {
     let prefix;
     const suffix = ' - Tc';
     var channel = settings.channels[settings.selectedTabIndex];
     if (channel) prefix = capitalize(channel);
     else prefix = 'Join channel';
     document.title = prefix + suffix;
-  });
+  }
 }
