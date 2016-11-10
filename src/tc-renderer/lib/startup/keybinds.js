337b26d47cb68f2545f969790e7af065ada117dc
mccxiv <adr.stella@gmail.com>
2016-11-09 23:05:54 -0600

    Add previous tab shortcut

--- src/tc-renderer/lib/startup/keybinds.js
+++ src/tc-renderer/lib/startup/keybinds.js
@@ -6,12 +6,19 @@ key.filter = () => true;
 function registerShortcuts() {
   key('⌘+n, ctrl+n', goToAddChannel);
   key('⌘+tab, ctrl+tab', nextTab);
+  key('⌘+shift+tab, ctrl+shift+tab', previousTab);
 }
 
 function nextTab() {
   const newIndex = settings.selectedTabIndex + 1;
   if (newIndex >= settings.channels.length) settings.selectedTabIndex = 0;
-  else settings.selectedTabIndex = newIndex
+  else settings.selectedTabIndex = newIndex;
+}
+
+function previousTab() {
+  const newIndex = settings.selectedTabIndex - 1;
+  if (newIndex < 0) settings.selectedTabIndex = settings.channels.length - 1;
+  else settings.selectedTabIndex = newIndex;
 }
 
 function goToAddChannel() {
