3b2ca3cc68965c38396146909858913c574306bc
mccxiv <adr.stella@gmail.com>
2016-11-09 22:54:33 -0600

    Add shortcut for switching to next tab

--- src/tc-renderer/lib/startup/keybinds.js
+++ src/tc-renderer/lib/startup/keybinds.js
@@ -1,8 +1,17 @@
 import key from 'keymaster';
 import settings from '../settings/settings';
 
+key.filter = () => true;
+
 function registerShortcuts() {
   key('⌘+n, ctrl+n', goToAddChannel);
+  key('⌘+tab, ctrl+tab', nextTab);
+}
+
+function nextTab() {
+  const newIndex = settings.selectedTabIndex + 1;
+  if (newIndex >= settings.channels.length) settings.selectedTabIndex = 0;
+  else settings.selectedTabIndex = newIndex
 }
 
 function goToAddChannel() {
