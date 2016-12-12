5bc6da9928b27450e6ac6ccae3086a1cb9599d59
mccxiv <adr.stella@gmail.com>
2016-12-11 21:06:24 -0600

    Re-fetch BTTV global emotes evey 12 hours

--- src/tc-renderer/lib/emotes/bttv.js
+++ src/tc-renderer/lib/emotes/bttv.js
@@ -11,15 +11,17 @@ getGlobal();
 channels.on('add', tryGrabbingChannel);
 channels.on('remove', (channel) => delete channelEmotes[channel]);
 channels.channels.forEach(tryGrabbingChannel);
+setInterval(getGlobal, 1000 * 60 * 60 * 6); // Re-fetch every 12 hours
 
-async function getGlobal(delay) {
-  await sleep(delay || 0);
+async function getGlobal(delay = 0, attempt = 1) {
+  await sleep(delay);
   try {await fetch()}
   catch (e) {
     delay = (delay || 1000) * 2;
     console.warn('BTTV: Couldn\'t grab global emotes.', e);
+    if (attempt > 30) return; // Give up until the next timed fetch
     console.warn(`BTTV: Retrying in ${delay/1000} seconds.`);
-    getGlobal(delay);
+    getGlobal(delay, attempt + 1);
   }
 }
 
