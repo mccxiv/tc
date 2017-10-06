fb353046dff2e7392b8a6e44d9b46ae31dfca982
Andrea Stella <adr.stella@gmail.com>
2017-10-06 18:33:36 +0800

    Fix emotes following emojis
    JS doesn't handle emojis well, let's use a library instead

--- src/tc-renderer/lib/emotes/twitch.js
+++ src/tc-renderer/lib/emotes/twitch.js
@@ -1,8 +1,10 @@
+import {substring} from 'stringz'
+
 export default function twitchEmotesToTcEmotesFormat(msg, emotesFromTwitch) {
   return Object.keys(emotesFromTwitch).map((id) => {
     const indexes = emotesFromTwitch[id][0].split('-').map(Number);
     return {
-      emote: msg.substring(indexes[0], indexes[1] + 1),
+      emote: substring(msg, indexes[0], indexes[1] + 1),
       url: `http://static-cdn.jtvnw.net/emoticons/v1/${id}/1.0`
     };
   });
