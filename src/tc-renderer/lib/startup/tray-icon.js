b2bcd6ccf011a839b569cc40c5396060370b03ea
josh <jleaver1989@gmail.com>
2017-12-18 17:06:57 -0500

    Fixes previous linux issues with minimize to tray. Now instead of just closing on linux, it will hide to tray, and you can restore/quit from tray

--- src/tc-renderer/lib/startup/tray-icon.js
+++ src/tc-renderer/lib/startup/tray-icon.js
@@ -1,17 +1,30 @@
 import icon16 from '../../../assets/icon16.png'
+import icon256 from '../../../assets/icon256.png'
 import settings from './../settings/settings'
 import electron from 'electron'
 
 let tray
+const Tray = electron.remote.Tray
+const nativeImage = electron.remote.nativeImage
+const Menu = electron.remote.Menu
+const app = electron.remote.app
+const ipcRenderer = electron.ipcRenderer
+const browserWindow = electron.remote.BrowserWindow
 
-export default function makeTrayIconOnWindows () {
-  if (process.platform !== 'win32') return null
-  const Tray = electron.remote.Tray
-  const nativeImage = electron.remote.nativeImage
-  const Menu = electron.remote.Menu
-  const app = electron.remote.app
-  const ipcRenderer = electron.ipcRenderer
-  const browserWindow = electron.remote.BrowserWindow
+export default function setupTrayIcon() {
+    switch (process.platform) {
+        case 'win32':
+            makeTrayIconOnWindows()
+            break
+        case 'linux':
+            makeTrayIconOnLinux()
+            break
+        default:
+            return null
+    }
+}
+
+function makeTrayIconOnWindows () {
   tray = new Tray(nativeImage.createFromDataURL(icon16))
 
   tray.on('click', () => browserWindow.getAllWindows()[0].show())
@@ -40,6 +53,24 @@ export default function makeTrayIconOnWindows () {
     const command = autoStart ? 'enable-auto-start' : 'disable-auto-start'
     ipcRenderer.send(command)
   }
+}
+
+function makeTrayIconOnLinux() {
+    const main = electron.remote.getCurrentWindow()
+    tray = new Tray(nativeImage.createFromDataURL(icon256))
+
+    tray.on('click', () => browserWindow.getAllWindows()[0].show())
+
+    tray.setContextMenu(Menu.buildFromTemplate([
+        {
+            label: 'Show Tc',
+            click: () => main.show()
+        },
+        {
+            label: 'Quit Tc',
+            click: () => app.quit()
+        }
+    ]))
 }
 
 export function getTray () {
