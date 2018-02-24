10902b4eb2f608f2a46762771eb542b3791e5e19
mccxiv <adr.stella@gmail.com>
2018-02-24 22:48:06 +0800

    Standardize gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,52 @@
-var shell = require('shelljs');
-var path = require('path');
-var gulp = require('gulp');
+const shell = require('shelljs')
+const path = require('path')
+const gulp = require('gulp')
 
-shell.config.throw = true; // Throw on error
+shell.config.throw = true // Throw on error
 
 gulp.task('dev-watch-files', function () {
-  shell.rm('-rf', '_build');
-  shell.mkdir('_build');
-  shell.cp('src/tc-renderer/index.html', '_build/index.html');
-  shell.cp('src/package.json', '_build/package.json');
-  shell.exec(path.normalize('./node_modules/.bin/webpack --watch'));
+  shell.rm('-rf', '_build')
+  shell.mkdir('_build')
+  shell.cp('src/tc-renderer/index.html', '_build/index.html')
+  shell.cp('src/package.json', '_build/package.json')
+  shell.exec(path.normalize('./node_modules/.bin/webpack --watch'))
 })
 
-gulp.task('dev-launch-electron', function() {
-  shell.exec(path.normalize('./node_modules/.bin/electron --enable-logging ./_build --dev-tools'));
-});
+gulp.task('dev-launch-electron', function () {
+  shell.exec(path.normalize(
+    './node_modules/.bin/electron --enable-logging ./_build --dev-tools'
+  ))
+})
 
 gulp.task('build', function() {
-  shell.rm('-rf', '_dist');
-  shell.mkdir('-p', '_dist');
-  shell.rm('-rf', '_build');
-  shell.exec(path.normalize('./node_modules/.bin/webpack'));
-  shell.cp('src/tc-renderer/index.html', '_build/index.html');
-  shell.cp('src/package.json', '_build/package.json');
-  console.log('Trying to build for Linux, it will fail on Windows');
-  shell.exec('npm run dist:linux');
-  console.log('Trying to build for Mac, it will fail unless on Mac');
-  shell.exec('npm run dist:mac');
-  console.log('Trying to build for Windows, it might fail without Wine');
-  shell.exec('npm run dist:windows');
-  shell.mv('dist/squirrel-windows/**', '_dist/');
-  shell.mv('dist/*.dmg', '_dist/');
-  shell.mv('dist/*.zip', '_dist/');
-  shell.mv('dist/*.AppImage', '_dist/');
+  shell.rm('-rf', '_dist')
+  shell.mkdir('-p', '_dist')
+  shell.rm('-rf', '_build')
+  shell.exec(path.normalize('./node_modules/.bin/webpack'))
+  shell.cp('src/tc-renderer/index.html', '_build/index.html')
+  shell.cp('src/package.json', '_build/package.json')
+  console.log('Trying to build for Linux, it will fail on Windows')
+  shell.exec('npm run dist:linux')
+  console.log('Trying to build for Mac, it will fail unless on Mac')
+  shell.exec('npm run dist:mac')
+  console.log('Trying to build for Windows, it might fail without Wine')
+  shell.exec('npm run dist:windows')
+  shell.mv('dist/squirrel-windows/**', '_dist/')
+  shell.mv('dist/*.dmg', '_dist/')
+  shell.mv('dist/*.zip', '_dist/')
+  shell.mv('dist/*.AppImage', '_dist/')
   shell.find('_dist').filter((f) => f.endsWith('.exe')).forEach((f) => {
-    shell.mv(f, f.replace('Tc Setup ', 'tc-setup-win-'));
-  });
+    shell.mv(f, f.replace('Tc Setup ', 'tc-setup-win-'))
+  })
   shell.find('_dist').filter((f) => f.endsWith('.dmg')).forEach((f) => {
-    shell.mv(f, f.replace('Tc', 'tc-setup-mac'));
-  });
+    shell.mv(f, f.replace('Tc', 'tc-setup-mac'))
+  })
   shell.find('_dist').filter((f) => f.endsWith('.zip')).forEach((f) => {
-    var newName = f.replace('-mac', '').replace('Tc', 'tc-mac');
-    shell.mv(f, newName);
-  });
+    const newName = f.replace('-mac', '').replace('Tc', 'tc-mac')
+    shell.mv(f, newName)
+  })
   shell.find('_dist').filter((f) => f.endsWith('.AppImage')).forEach((f) => {
-    shell.mv(f, f.replace('Tc-', 'tc-linux-').replace('-x86_64', ''));
-  });
-  shell.rm('-rf', 'dist');
-});
+    shell.mv(f, f.replace('Tc-', 'tc-linux-').replace('-x86_64', ''))
+  })
+  shell.rm('-rf', 'dist')
+})
