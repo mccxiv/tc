bb731440db81a7fb0647aded93715618ae5d4b5b
Bauke <19501722+Bauke@users.noreply.github.com>
2018-04-19 23:59:00 +0200

    Add \ to escape channels on launch

--- src/tc-renderer/lib/startup/commandline.js
+++ src/tc-renderer/lib/startup/commandline.js
@@ -6,7 +6,7 @@ export default function joinChannelFromCommand() {
     electron.remote.process.argv.forEach(function (arg) {
         if (arg.includes('--channel=')) {
             setTimeout(function () {
-                main.webContents.send('join-channel', arg.slice(10))
+                main.webContents.send('join-channel', arg.slice(10).replace(/\\/g, ''))
             }, 1500)
         }
     })
