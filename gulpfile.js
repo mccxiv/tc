0f4590dd08f00c541bc9c3da0f0ed4432202bf59
Bauke <19501722+Bauke@users.noreply.github.com>
2018-04-27 23:05:04 +0200

    Refactor building so we only build on the platforms that are possible

--- gulpfile.js
+++ gulpfile.js
@@ -27,28 +27,53 @@ gulp.task('build', function () {
   shell.exec(path.normalize('./node_modules/.bin/webpack --mode production'))
   shell.cp('src/tc-renderer/index.html', '_build/index.html')
   shell.cp('src/package.json', '_build/package.json')
-  console.log('Trying to build for Linux, it will fail on Windows')
-  shell.exec('npm run dist:linux')
-  console.log('Trying to build for Mac, it will fail unless on Mac')
-  shell.exec('npm run dist:mac')
-  console.log('Trying to build for Windows, it might fail without Wine')
-  shell.exec('npm run dist:windows')
-  shell.mv('dist/squirrel-windows/*', '_dist/')
-  shell.mv('dist/*.dmg', '_dist/')
-  shell.mv('dist/*.zip', '_dist/')
-  shell.mv('dist/*.AppImage', '_dist/')
-  shell.find('_dist').filter((f) => f.endsWith('.exe')).forEach((f) => {
-    shell.mv(f, f.replace('Tc Setup ', 'tc-setup-win-'))
-  })
-  shell.find('_dist').filter((f) => f.endsWith('.dmg')).forEach((f) => {
-    shell.mv(f, f.replace('Tc', 'tc-setup-mac'))
-  })
-  shell.find('_dist').filter((f) => f.endsWith('.zip')).forEach((f) => {
-    const newName = f.replace('-mac', '').replace('Tc', 'tc-mac')
-    shell.mv(f, newName)
-  })
-  shell.find('_dist').filter((f) => f.endsWith('.AppImage')).forEach((f) => {
-    shell.mv(f, f.replace('Tc-', 'tc-linux-').replace('-x86_64', ''))
-  })
+  switch (process.platform) {
+    case 'win32':
+      console.log('\nAttempting to build Windows version.')
+      build('windows')
+      break
+    case 'linux':
+      console.log('\nAttempting to build Linux version.')
+      build('linux')
+      console.log('\nAttempting to build Windows version.')
+      console.log('This will fail without Wine installed!')
+      build('windows')
+      break
+    case 'darwin':
+      console.log('\nAttempting to build Mac version.')
+      build('mac')
+      console.log('\nAttempting to build Linux version.')
+      build('linux') // TBD if possible
+      console.log('\nAttempting to build Windows version.')
+      build('windows') // TBD if possible
+      break
+  }
   shell.rm('-rf', 'dist')
 })
+
+function build (platform) {
+  if (platform === 'windows') {
+    shell.exec('npm run dist:windows')
+    shell.mv('dist/squirrel-windows/*', '_dist/')
+    shell.find('_dist').filter((f) => f.endsWith('.exe')).forEach((f) => {
+      shell.mv(f, f.replace('Tc Setup ', 'tc-setup-win-'))
+    })
+  } else if (platform === 'linux') {
+    shell.exec('npm run dist:linux')
+    shell.mv('dist/*.AppImage', '_dist/')
+    shell.find('_dist').filter((f) => f.endsWith('.AppImage')).forEach((f) => {
+      shell.mv(f, f.replace('Tc-', 'tc-linux-').replace('-x86_64', ''))
+    })
+  } else if (platform === 'mac') {
+    shell.exec('npm run dist:mac')
+    shell.mv('dist/*.dmg', '_dist/')
+    shell.mv('dist/*.zip', '_dist/')
+    shell.find('_dist').filter((f) => f.endsWith('.dmg')).forEach((f) => {
+      shell.mv(f, f.replace('Tc', 'tc-setup-mac'))
+    })
+    shell.find('_dist').filter((f) => f.endsWith('.zip')).forEach((f) => {
+      const newName = f.replace('-mac', '').replace('Tc', 'tc-mac')
+      shell.mv(f, newName)
+    })
+  }
+}
