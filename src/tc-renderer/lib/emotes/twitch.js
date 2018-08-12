354f5e0644e18914561b1ece53cf7b0eb20c8c8c
mccxiv <adr.stella@gmail.com>
2018-08-12 11:12:49 +0800

    Fix typo

--- src/tc-renderer/lib/emotes/twitch.js
+++ src/tc-renderer/lib/emotes/twitch.js
@@ -4,7 +4,7 @@ export default function twitchToTcEmotes (msg, emotesFromTwitch, isOutgoing) {
   return Object.keys(emotesFromTwitch).map((id) => {
     const indexes = emotesFromTwitch[id][0].split('-').map(Number)
     return {
-      // Outgoing messsage emotes are parsed by the library instead of by Twitch
+      // Outgoing message emotes are parsed by the library instead of by Twitch
       // Unfortunately their implementations differ, so we handle differently
       emote: isOutgoing
         ? msg.slice(indexes[0], indexes[1] + 1)
