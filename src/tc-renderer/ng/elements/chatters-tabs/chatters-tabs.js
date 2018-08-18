9c08c77aaf19a828cef4331cf02661909eb20c86
mccxiv <adr.stella@gmail.com>
2018-08-18 17:01:54 +0800

    Componentize

--- src/tc-renderer/ng/elements/chatters-tabs/chatters-tabs.js
+++ src/tc-renderer/ng/elements/chatters-tabs/chatters-tabs.js
@@ -2,20 +2,15 @@ import './chatters-tabs.styl'
 import angular from 'angular'
 import template from './chatters-tabs.pug'
 
-angular.module('tc').directive('chattersTabs', (store) => {
-  return {
-    restrict: 'E',
-    template: template,
-    scope: {},
-    link: (scope) => {
-      scope.settings = store.settings.state
-      scope.hideChatters = () => {} // TODO why?
-      scope.toggleButtonClasses = () => {
-        return {
-          'hide-button': scope.settings.appearance.chatters,
-          collapsed: !scope.settings.appearance.chatters
-        }
-      }
+angular.module('tc').component('chattersTabs', {template, controller})
+
+function controller (store) {
+  const vm = this
+  vm.settings = store.settings.state
+  vm.toggleButtonClasses = () => {
+    return {
+      'hide-button': vm.settings.appearance.chatters,
+      collapsed: !vm.settings.appearance.chatters
     }
   }
-})
+}
