6373d0ac3a74d7208778ed0054f02fe5d3670516
mccxiv <adr.stella@gmail.com>
2018-08-18 16:43:41 +0800

    Try out component

--- src/tc-renderer/ng/elements/chatters/chatters.js
+++ src/tc-renderer/ng/elements/chatters/chatters.js
@@ -2,65 +2,65 @@ import './chatters.styl'
 import angular from 'angular'
 import template from './chatters.pug'
 import {getChattersApi} from '../../../lib/chatters'
-import channels from '../../../lib/channels'
 import prettyChatterNames from '../../../lib/transforms/pretty-chatter-names'
 
-angular.module('tc').directive('chatters', ($http, session, store) => {
-  function link (scope) {
-    let forceShowViewers = false
-    let timeout = null
-    scope.api = null
-    scope.searchText = ''
+angular.module('tc').component('chatters', {
+  template,
+  controller,
+  bindings: {channel: '<'}
+})
 
-    fetchList()
-    setInterval(fetchList, 120000)
+function controller ($http, $scope, session, store) {
+  let forceShowViewers = false
+  let timeout = null
+  const vm = this
+  vm.api = null
+  vm.searchText = ''
+  vm.prettyChatterNames = prettyChatterNames
+  vm.showViewers = showViewers
+  vm.tooManyNotDoable = tooManyNotDoable
+  vm.selectUser = selectUser
 
-    channels.on('change', function () {
-      forceShowViewers = false
-      if (!scope.api) timeoutFetch(200)
-      else timeoutFetch(2000)
-    })
+  fetchList(800)
+  const reFetchInterval = setInterval(fetchList, 120000)
+  $scope.$watch(isChannelSelected, () => timeoutFetch(800))
+  $scope.$on('$destroy', () => clearInterval(reFetchInterval))
 
-    scope.prettyChatterNames = prettyChatterNames
-
-    scope.showViewers = function (force) {
-      if (typeof force === 'boolean') forceShowViewers = force
-      if (!scope.api) return false
-      if (scope.searchText.length > 1) return true
-      if (scope.api.chatters.viewers.length < 201) return true
-      else return forceShowViewers
-    }
-
-    scope.tooManyNotDoable = function () {
-      if (scope.searchText.length > 1) return false // Show all if searching
-      return scope.api && scope.api.chatters.viewers.length > 10000
-    }
-
-    scope.selectUser = function (username) {
-      session.selectedUser = username
-      session.selectedUserChannel = scope.channel
-    }
-
-    async function fetchList (attemptNumber) {
-      if (!isChannelSelected()) return
-      try { scope.api = await getChattersApi(scope.channel) } catch (e) {
-        attemptNumber = attemptNumber || 1
-        console.warn('CHATTERS: Failed to get user list. ' + attemptNumber, e)
-        if (attemptNumber < 6) fetchList(attemptNumber + 1)
-      }
-      scope.$apply()
-    }
-
-    function isChannelSelected () {
-      const settings = store.settings.state
-      return settings.channels[settings.selectedTabIndex] === scope.channel
-    }
-
-    function timeoutFetch (duration) {
-      clearTimeout(timeout)
-      timeout = setTimeout(fetchList, duration)
-    }
+  function showViewers (force) {
+    if (typeof force === 'boolean') forceShowViewers = force
+    if (!vm.api) return false
+    if (vm.searchText.length > 1) return true
+    if (vm.api.chatters.viewers.length < 201) return true
+    else return forceShowViewers
   }
 
-  return {restrict: 'E', template, link, scope: {channel: '='}}
-})
+  function tooManyNotDoable () {
+    if (vm.searchText.length > 1) return false // Show all if searching
+    return vm.api && vm.api.chatters.viewers.length > 10000
+  }
+
+  function selectUser (username) {
+    session.selectedUser = username
+    session.selectedUserChannel = vm.channel
+  }
+
+  async function fetchList (attemptNumber) {
+    if (!isChannelSelected()) return
+    try { vm.api = await getChattersApi(vm.channel) } catch (e) {
+      attemptNumber = attemptNumber || 1
+      console.warn('CHATTERS: Failed to get user list. ' + attemptNumber, e)
+      if (attemptNumber < 6) fetchList(attemptNumber + 1)
+    }
+    $scope.$digest()
+  }
+
+  function isChannelSelected () {
+    const settings = store.settings.state
+    return settings.channels[settings.selectedTabIndex] === vm.channel
+  }
+
+  function timeoutFetch (duration) {
+    clearTimeout(timeout)
+    timeout = setTimeout(fetchList, duration)
+  }
+}
