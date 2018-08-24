fb4d9557c7ea4669c899adfbd06056e90ce898cf
mccxiv <adr.stella@gmail.com>
2018-08-24 21:02:46 +0800

    Convert to component

--- src/tc-renderer/ng/elements/emote-menu/emote-menu.js
+++ src/tc-renderer/ng/elements/emote-menu/emote-menu.js
@@ -4,49 +4,50 @@ import angular from 'angular'
 import template from './emote-menu.pug'
 import {getAllCachedEmotes} from '../../../lib/emotes/menu'
 
-angular.module('tc').directive('emoteMenu', ($timeout, session, settings) => {
-  function link (scope, element) {
-    scope.m = {
-      visible: false,
-      categories: getAllCachedEmotes(currChannel())
-    }
+angular.module('tc').component('emoteMenu', {template, controller})
+
+function controller ($element, $timeout, session, settings) {
+  const vm = this
+
+  vm.$onInit = () => {
+    vm.visible = false
+    vm.categories = getAllCachedEmotes(currChannel())
+    vm.choose = choose
 
     handleEmoteHover()
-    $timeout(() => scope.m.visible = true)
-
-    scope.choose = function (emote) {
-      const space = session.message ? ' ' : ''
-      session.message = `${session.message || ''}${space}${emote} `
-      session.input.focus()
-    }
-
-    function handleEmoteHover () {
-      $(element[0]).on('mouseenter', '.emoticon', e => {
-        const emoticon = $(e.target)
-        let tooltip = emoticon.data('emote-name')
-        const description = emoticon.data('emote-description')
-
-        if (description) tooltip += '<br>' + description
-        showTooltip(emoticon, tooltip)
-      })
-    }
-
-    function currChannel () {
-      return settings.channels[settings.selectedTabIndex]
-    }
-
-    function showTooltip (el, content) {
-      el.frosty({html: true, content})
-      el.frosty('show')
-      el.one('mouseleave', kill)
-      setTimeout(kill, 3000)
-
-      function kill () {
-        el.frosty('hide')
-        el.off()
-      }
-    }
+    $timeout(() => vm.visible = true)
   }
 
-  return {restrict: 'E', template, link, scope: {}}
-})
+  vm.$onDestroy = () => $($element[0]).off()
+
+  const choose = function (emote) {
+    const space = session.message ? ' ' : ''
+    session.message = `${session.message || ''}${space}${emote} `
+    session.input.focus()
+  }
+
+  const handleEmoteHover = () => {
+    $($element[0]).on('mouseenter', '.emoticon', e => {
+      const emoticon = $(e.target)
+      let tooltip = emoticon.data('emote-name')
+      const description = emoticon.data('emote-description')
+
+      if (description) tooltip += '<br>' + description
+      showTooltip(emoticon, tooltip)
+    })
+  }
+
+  const currChannel = () => settings.channels[settings.selectedTabIndex]
+
+  const showTooltip = (el, content) => {
+    el.frosty({html: true, content})
+    el.frosty('show')
+    el.one('mouseleave', kill)
+    setTimeout(kill, 3000)
+
+    function kill () {
+      el.frosty('destroy')
+      el.off()
+    }
+  }
+}
