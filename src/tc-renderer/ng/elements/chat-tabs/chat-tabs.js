689bd44a3be313c983d4ee2939d58bd335feefa5
mccxiv <adr.stella@gmail.com>
2018-08-24 20:43:19 +0800

    Convert to component

--- src/tc-renderer/ng/elements/chat-tabs/chat-tabs.js
+++ src/tc-renderer/ng/elements/chat-tabs/chat-tabs.js
@@ -1,158 +1,128 @@
 import './chat-tabs.styl'
-import $ from 'jquery'
 import angular from 'angular'
 import template from './chat-tabs.pug'
 import channels from '../../../lib/channels'
 import * as api from '../../../lib/api'
+import * as R from 'ramda'
 
-angular.module('tc').directive('chatTabs', ($timeout, messages, settings) => {
-  function link (scope, element) {
-    const getStreamsInterval = setInterval(getStreams, 60000)
-    element = $(element[0])
+angular.module('tc').component('chatTabs', {template, controller})
 
-    scope.m = {
-      streams: {},
-      hotkey: process.platform === 'darwin' ? '⌘' : 'ctrl'
-    }
-    scope.settings = settings
-    scope.hidden = {}
-    scope.loaded = {}
-    scope.readUntil = {}
-    element.attr('layout', 'column')
+function controller ($scope, $timeout, messages, settings) {
+  const vm = this
 
-    scope.selected = (channel) => load(channel, true)
-    scope.deselected = handleChannelDeselected
-    scope.unread = numberOfUnreadMessages
-    scope.showingAddChannel = isAddTabSelected
-    scope.moveLeft = moveLeft
-    scope.moveRight = moveRight
-    scope.live = liveStreamType
+  vm.$onInit = () => {
+    vm.streams = {}
+    vm.hotkey = process.platform === 'darwin' ? '⌘' : 'ctrl'
+    vm.settings = settings
+    vm.loaded = []
+    vm.readUntil = {}
+    vm.getStreamsInterval = setInterval(getStreams, 60000)
 
-    scope.$on('$destroy', cleanup)
+    vm.unread = numberOfUnreadMessages
+    vm.showingAddChannel = isAddTabSelected
+    vm.moveLeft = moveLeft
+    vm.moveRight = moveRight
+    vm.live = liveStreamType
+    vm.isLoaded = isLoaded
 
-    if (currChannel()) scope.loaded[currChannel()] = true
-
+    loadCurrentChannel()
     getStreams()
+    channels.on('change', getStreams)
 
-    channels.on('change', () => {
-      getStreams()
-    })
+    $scope.$watchCollection(
+      currChannel,
+      R.pipe(loadCurrentChannel, unloadNonCurrentChannelsDelayed)
+    )
+  }
 
-    // TODO remove hack. When joining a new channel it won't render unless...
-    channels.on('add', () => {
-      setTimeout(() => clickTab(settings.channels.length), 10)
-      setTimeout(() => clickTab(settings.channels.length - 1), 200)
-    })
+  vm.$onDestroy = () => clearInterval(vm.getStreamsInterval)
 
-    function moveLeft ($event, channel) {
-      moveTab($event, channel, -1)
-    }
+  const loadCurrentChannel = () => {
+    vm.loaded = R.pipe(R.append(currChannel()), R.uniq)(vm.loaded)
+  }
 
-    function moveRight ($event, channel) {
-      moveTab($event, channel, 1)
-    }
+  function unloadNonCurrentChannelsDelayed () {
+    R.pipe(
+      R.without(currChannel()),
+      R.map(
+        R.pipe(
+          unloadInThreeSeconds,
+          countUnreadMessages
+        )
+      )
+    )(vm.loaded)
+  }
 
-    function moveTab ($event, channel, positionChange) {
-      const index = settings.channels.indexOf(channel)
-      const newIndex = index + positionChange
-      arrayMove(settings.channels, index, newIndex)
-      $event.stopPropagation()
-      $event.preventDefault()
-    }
-
-    function clickTab (index) {
-      element.find('md-tab-item').eq(index).click()
-    }
-
-    /**
-     * Returns how many unread messages a channel has.
-     * @param channel
-     * @returns {string|number}
-     */
-    function numberOfUnreadMessages (channel) {
-      if (currChannel() === channel) return ''
-      let unread = messages(channel).counter - (scope.readUntil[channel] || 0)
-      if (!unread) return ''
-      if (unread > 100) return '*'
-      else return unread
-    }
-
-    function isAddTabSelected () {
-      return settings.selectedTabIndex === settings.channels.length
-    }
-
-    function handleChannelDeselected (channel) {
-      const lines = messages(channel)
-      load(channel, false)
-      hideTemporarily(channel)
-      if (lines) scope.readUntil[channel] = lines.counter
-      else delete scope.readUntil[channel] // Channel was left
-    }
-
-    /**
-     * The chat-output directive should not be shown and hidden immediately
-     * because it's a very CPU intensive operation, let the animations
-     * run first then do the heavy DOM manipulation.
-     * @param {string} channel
-     * @param {boolean} show
-     */
-    function load (channel, show) {
-      // $timeout(loadSync, show? 1000 : 3000);
-      loadSync()
-
-      function loadSync () {
-        // Abort unload operation if the tab to be hidden is selected again
-        if (currChannel() === channel && !show) return
-        if (show) scope.loaded[channel] = true
-        else {
-          $timeout(() => {
-            if (currChannel() !== channel) {
-              delete scope.loaded[channel]
-            }
-          }, 3000)
-        }
+  function unloadInThreeSeconds (channel) {
+    console.log('unloading', channel)
+    $timeout(() => {
+      if (currChannel() !== channel) {
+        vm.loaded = R.without(channel, vm.loaded)
       }
-    }
+    }, 3000)
+  }
 
-    /**
-     * Hide the exiting channel first, then remove it from DOM.
-     * Removing it immediately uses too much CPU
-     * @param {string} channel
-     */
-    function hideTemporarily (channel) {
-      scope.hidden[channel] = true
-      $timeout(() => delete scope.hidden[channel], 1500)
-    }
+  function moveLeft ($event, channel) {
+    moveTab($event, channel, -1)
+  }
 
-    function currChannel () {
-      return settings.channels[settings.selectedTabIndex]
-    }
+  function moveRight ($event, channel) {
+    moveTab($event, channel, 1)
+  }
 
-    function arrayMove (arr, fromIndex, toIndex) {
-      const element = arr[fromIndex]
-      arr.splice(fromIndex, 1)
-      arr.splice(toIndex, 0, element)
-    }
+  function moveTab ($event, channel, positionChange) {
+    const index = settings.channels.indexOf(channel)
+    const newIndex = index + positionChange
+    arrayMove(settings.channels, index, newIndex)
+    $event.stopPropagation()
+    $event.preventDefault()
+  }
 
-    async function getStreams() {
-      for (const channel of settings.channels) {
-        scope.m.streams[channel] = await api.stream(channel)
-      }
-    }
+  function isLoaded (channel) {
+    return R.contains(channel, vm.loaded)
+  }
 
-    function liveStreamType(channel) {
-      if (
-        !scope.m.streams[channel] ||
-        !scope.m.streams[channel].stream
-      ) return null
+  /**
+   * Returns how many unread messages a channel has.
+   * @param channel
+   * @returns {string|number}
+   */
+  function numberOfUnreadMessages (channel) {
+    if (currChannel() === channel) return ''
+    let unread = messages(channel).counter - (vm.readUntil[channel] || 0)
+    if (!unread) return ''
+    if (unread > 100) return '*'
+    else return unread
+  }
 
-      return scope.m.streams[channel].stream.stream_type
-    }
+  function isAddTabSelected () {
+    return settings.selectedTabIndex === settings.channels.length
+  }
 
-    function cleanup() {
-      clearInterval(getStreamsInterval)
+  function countUnreadMessages (channel) {
+    const lines = messages(channel)
+    if (lines) vm.readUntil[channel] = lines.counter
+    else delete vm.readUntil[channel] // Channel was left
+  }
+
+  function currChannel () {
+    return settings.channels[settings.selectedTabIndex]
+  }
+
+  function arrayMove (arr, fromIndex, toIndex) {
+    const element = arr[fromIndex]
+    arr.splice(fromIndex, 1)
+    arr.splice(toIndex, 0, element)
+  }
+
+  async function getStreams () {
+    for (const channel of settings.channels) {
+      vm.streams[channel] = await api.stream(channel)
     }
   }
 
-  return {restrict: 'E', template, link, scope: {}}
-})
+  function liveStreamType (channel) {
+    if (!vm.streams[channel] || !vm.streams[channel].stream) return null
+    return vm.streams[channel].stream.stream_type
+  }
+}
