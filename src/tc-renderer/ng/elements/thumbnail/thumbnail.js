576369c82a22d2eff12fbdcfe55cc4f9fbcdbaf6
mccxiv <adr.stella@gmail.com>
2018-08-24 23:18:11 +0800

    Heck yeah

--- src/tc-renderer/ng/elements/thumbnail/thumbnail.js
+++ src/tc-renderer/ng/elements/thumbnail/thumbnail.js
@@ -7,13 +7,16 @@ import template from './thumbnail.pug'
 import * as api from '../../../lib/api'
 import channels from '../../../lib/channels'
 
-angular.module('tc').directive('thumbnail', (
-  irc, messages, openExternal, settings
-) => {
-  function link (scope, element) {
-    const loadThumbnailInterval = setInterval(loadThumbnail, 60000)
-    const loadHostStatusInterval = setInterval(loadHostStatus, 60 * 5 * 1000)
-    scope.m = {
+angular.module('tc').component('thumbnail', {template, controller})
+
+function controller ($scope, $element, irc, messages, openExternal, settings) {
+  const vm = this
+
+  vm.$onInit = () => {
+    vm.loadThumbnailInterval = setInterval(loadThumbnail, 60000)
+    vm.loadHostStatusInterval = setInterval(loadHostStatus, 60 * 5 * 1000)
+    vm.settings = settings
+    vm.m = {
       img: '',
       channel: null,
       stream: null,
@@ -27,108 +30,112 @@ angular.module('tc').directive('thumbnail', (
     checkStreamlinkInstallation()
     checkLivestreamerInstallation()
 
-    element.attr('layout', 'column')
+    $element.attr('layout', 'column')
 
+    // TODO memory leak
     channels.on('change', () => {
-      scope.m.stream = null
-      scope.m.channel = null
-      scope.m.hosting = false
+      vm.m.stream = null
+      vm.m.channel = null
+      vm.m.hosting = false
       loadThumbnail()
       loadHostStatus()
     })
+  }
 
-    scope.$on('$destroy', cleanup)
+  vm.$onDestroy = () => {
+    clearInterval(vm.loadThumbnailInterval)
+    clearInterval(vm.loadHostStatusInterval)
+  }
 
-    scope.host = () => {
-      irc.say(`#${settings.identity.username}`, `.host ${getChannel()}`)
-      messages.addNotification(getChannel(), 'Host command sent.')
-      setTimeout(loadHostStatus, 1500)
-      setTimeout(loadHostStatus, 4000)
-    }
+  vm.host = () => {
+    irc.say(`#${settings.identity.username}`, `.host ${getChannel()}`)
+    messages.addNotification(getChannel(), 'Host command sent.')
+    setTimeout(loadHostStatus, 1500)
+    setTimeout(loadHostStatus, 4000)
+  }
 
-    scope.unhost = () => {
-      irc.say(`#${settings.identity.username}`, '.unhost')
-      messages.addNotification(getChannel(), 'Unhost command sent.')
-      setTimeout(loadHostStatus, 1500)
-      setTimeout(loadHostStatus, 4000)
-    }
+  vm.unhost = () => {
+    irc.say(`#${settings.identity.username}`, '.unhost')
+    messages.addNotification(getChannel(), 'Unhost command sent.')
+    setTimeout(loadHostStatus, 1500)
+    setTimeout(loadHostStatus, 4000)
+  }
 
-    scope.playMediaplayer = audioOnly => {
-      const player = scope.m.streamlink ? 'streamlink' : 'livestreamer'
-      const type = audioOnly ? 'audio_only' : ''
-      const channel = 'twitch.tv/' + getChannel()
-      stream(type)
+  vm.playMediaplayer = audioOnly => {
+    const player = vm.m.streamlink ? 'streamlink' : 'livestreamer'
+    const type = audioOnly ? 'audio_only' : ''
+    const channel = 'twitch.tv/' + getChannel()
+    stream(type)
 
-      function stream (quality) {
-        const id = '1pr5dzvymq1unqa2xiavdkvslsn4ebe'
-        exec(`${player} --http-header Client-ID=${id} ${channel} ${quality}`,
-          (err, stdout) => {
-            if (!err && stdout) {
-              const lastLine = stdout.trim().split('\n').pop()
-              if (lastLine.startsWith('Available streams')) {
-                stream('best')
-              }
+    function stream (quality) {
+      const id = '1pr5dzvymq1unqa2xiavdkvslsn4ebe'
+      exec(`${player} --http-header Client-ID=${id} ${channel} ${quality}`,
+        (err, stdout) => {
+          if (!err && stdout) {
+            const lastLine = stdout.trim().split('\n').pop()
+            if (lastLine.startsWith('Available streams')) {
+              stream('best')
             }
           }
-        )
-      }
-    }
-
-    scope.playTwitch = () => {
-      openExternal(`http://www.twitch.tv/${getChannel()}/popout`)
-    }
-
-    function checkStreamlinkInstallation () {
-      which('streamlink', err => scope.m.streamlink = !err)
-    }
-
-    function checkLivestreamerInstallation () {
-      which('livestreamer', err => scope.m.livestreamer = !err)
-    }
-
-    function getChannel () {
-      return settings.channels[settings.selectedTabIndex]
-    }
-
-    function cleanup () {
-      clearInterval(loadThumbnailInterval)
-      clearInterval(loadHostStatusInterval)
-    }
-
-    async function loadThumbnail () {
-      const channel = getChannel()
-      scope.m.channel = await api.channel(channel)
-      scope.m.stream = (await api.stream(channel)).stream
-      if (!scope.m.stream) return
-      const url = scope.m.stream.preview.medium + '?' + new Date().getTime()
-      await preLoadImage(url)
-      scope.m.img = url
-      scope.$apply()
-    }
-
-    function loadHostStatus () {
-      // This task is lower priority than the others, let them run first.
-      setTimeout(async () => {
-        const user = (irc.getClient() || {}).globaluserstate
-        const id = user && user['user-id'] ? user['user-id'] : null
-        if (!id) return setTimeout(loadHostStatus, 2000)
-        const opts = {params: {host: id, include_logins: 1}}
-        const resp = await axios('https://tmi.twitch.tv/hosts', opts)
-        if (resp.data && resp.data.hosts[0]) {
-          scope.m.hosting = resp.data.hosts[0].target_login === getChannel()
-          scope.$apply()
         }
-      }, 600)
-    }
-
-    async function preLoadImage (url) {
-      return new Promise((resolve) => {
-        const img = new window.Image()
-        img.src = url
-        img.addEventListener('load', resolve)
-      })
+      )
     }
   }
 
-  return {restrict: 'E', template, link, scope: {}}
-})
+  vm.playTwitch = () => {
+    openExternal(`http://www.twitch.tv/${getChannel()}/popout`)
+  }
+
+  function checkStreamlinkInstallation () {
+    which('streamlink', err => vm.m.streamlink = !err)
+  }
+
+  function checkLivestreamerInstallation () {
+    which('livestreamer', err => vm.m.livestreamer = !err)
+  }
+
+  function getChannel () {
+    return settings.channels[settings.selectedTabIndex]
+  }
+
+  async function loadThumbnail () {
+    const channel = getChannel()
+    vm.m.channel = await api.channel(channel)
+    vm.m.stream = (await api.stream(channel)).stream
+    if (!vm.m.stream) return
+    const url = vm.m.stream.preview.medium + '?' + new Date().getTime()
+    await preLoadImage(url)
+    vm.m.img = url
+    $scope.$digest()
+  }
+
+  function loadHostStatus () {
+    // This task is lower priority than the others, let them run first.
+    setTimeout(async () => {
+      const user = (irc.getClient() || {}).globaluserstate
+      const id = user && user['user-id'] ? user['user-id'] : null
+      if (!id) return setTimeout(loadHostStatus, 2000)
+      const opts = {params: {host: id, include_logins: 1}}
+      const resp = await axios('https://tmi.twitch.tv/hosts', opts)
+      if (resp.data && resp.data.hosts[0]) {
+        vm.m.hosting = resp.data.hosts[0].target_login === getChannel()
+        $scope.$digest()
+      }
+    }, 600)
+  }
+
+  async function preLoadImage (url) {
+    return new Promise((resolve) => {
+      const img = new window.Image()
+      img.src = url
+      img.addEventListener('load', done)
+      img.addEventListener('error', done)
+
+      function done () {
+        img.removeEventListener('load', done)
+        img.removeEventListener('error', done)
+        resolve()
+      }
+    })
+  }
+}
